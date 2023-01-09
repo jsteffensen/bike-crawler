1c5242d1604d1725af4a04ee020a450a4c72e35c
jsteffensen <jsteffensen@gmail.com>
2023-01-09 15:37:30 +0100

    switch to puppeteer

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
 // bike-crawler
-const https = require('https');
-const bl = require('bl');
-const parser = require('node-html-parser');
+/*
+https://www.scott-sports.com/us/en/sports/bike
+https://www.canyon.com/en-us/
+https://www.trekbikes.com/us/en_US/
+https://www.specialized.com/us/en/shop/bikes/c/bikes
+https://www.giant-bicycles.com/us
+https://www.cannondale.com/en-us
+https://www.santacruzbicycles.com/en-US
+https://www.merida-bikes.com/en
+https://www.orbea.com/us-en/
+https://www.bmc-switzerland.com/us_en/
+*/
 
-let url = 'https://www.specialized.com/dk/en/shop/bikes/road-bikes/adventure--gravel-bikes/c/roadadventuregravel';
+const puppeteer = require('puppeteer')
+async function scrape() {
+    const browser = await puppeteer.launch({});
+    const page = await browser.newPage();
 
-httpsGet(url).then((data)=>{
-    //parse(data);
-}, (e)=>{
-    console.log(e);
-});
-
-
-function parse(data){
-
-    let page = parser.parse(data);
-    console.log(page.toString());
-    //let owlStage = page.querySelector('.owl-stage');
-    /*let owlItem = owlStage.querySelectorAll('.owl-item');
-
-    owlItem.forEach((item)=>{
-        let modelUrl = item.querySelector('a');
-        console.log(modelUrl.getAttribute('href'));
-    });*/
-
+    await page.goto('https://www.scott-sports.com/us/en/products/bike-bikes-road-gravel-cx');
+    var element = await page.waitForSelector('div#content');
+    var text = await page.evaluate(element => element.textContent, element);
+    console.log(text);
+    browser.close();
 }
 
-async function httpsGet(url) {
-    return new Promise((resolve, reject) => {
-        https.get(url, response => {
-            response.setEncoding('utf8');
-            response.pipe(bl((err, data) => {
-                if (err) {
-                    reject(err);
-                }
-                if(response.statusCode == 404) {
-                    reject('Page does not exist');
-                }
-                resolve(data.toString());
-            }));
-        });
-    });
-}
+scrape();
